12b109e4e8f8eabc8e9f0787369320ddfb77fe14
Shernna <123427707+Shernna@users.noreply.github.com>
2024-07-09 21:17:18 -0600

    Semana 08

---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,47 @@
+
+package com.tienda.domain;
+  
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data // Automaticamente crear los set y get
+@Entity
+@Table (name="producto")
+
+public class Producto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue (strategy = GenerationType. IDENTITY)
+    @Column (name = "id_producto")
+    private Long idProducto;
+
+    public Producto(Long idProducto, String descripcion, String detalle, double precio, int existencias, String rutaImagen, boolean activo, Long idCategoria) {
+        this.idProducto = idProducto;
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+        this.idCategoria = idCategoria;
+    }
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String rutaImagen; 
+    private boolean activo;
+    private Long idCategoria;
+    }
+
+
+    
+
